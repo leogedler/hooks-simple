0e4d4c5f172ccac86f115bd4ac3aa5c2fd86c980
Leonardo Gedler <leonardogedler@gmail.com>
2019-08-07 00:09:09 -0400

    WIP

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const App = () => {
+
+  const [resource, setResource] = useState('posts');
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('todos')}>Todos</button>
+        {resource}
+      </div>
+    </div>
+  );
+
+}
+
+export default App;
